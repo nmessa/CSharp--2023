94f13c4ff4fdb28e2b4306c92ee7aa63c7b3d779
nmessa <nmessa@sau16.org>
2023-02-10 12:49:10 -0500

    Add files via upload

---
+++ Lab Exercise 2.15.2023/Stadium/Stadium/Form1.cs
@@ -0,0 +1,40 @@
+﻿//Lab Exercise 2.21.2022 Problem 2
+//Author: 
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Stadium
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //Add code here
+
+        }
+    }
+}
